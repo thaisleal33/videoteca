9b7720830c3b400796739d415e0e0daf5eb8b9a8
thaisleal33 <thaislealto@gmail.com>
2022-07-07 06:39:59 -0300

    initial

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Footer() {
+  return (
+    <Container>
+      <strong>Simple Study app developed with ❤ by @thaisleal33</strong>
+    </Container>
+  );
+}
